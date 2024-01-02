ee6bc05c89b528b35871fbb119003f74430578c3
valentindjangone <djangone.valentin@gmail.com>
2024-01-02 13:38:58 +0100

    auth deleted

--- routes/data_analysis_route.py
+++ routes/data_analysis_route.py
@@ -1,52 +1,24 @@
 from fastapi import APIRouter, HTTPException, Query
 from services.data_analysis_service import get_wordcount, get_stats
-from fastapi import APIRouter, HTTPException, Query, Depends
-from fastapi.security import OAuth2PasswordBearer
-import jwt
-from jwt import PyJWTError
-from pydantic import BaseModel
-from typing import Optional
 
-# Configuration du schéma de sécurité OAuth2 avec Password (et Bearer)
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+router = APIRouter(prefix ='/data-analysis', tags=["Data Analysis"])
 
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-# Fonction pour vérifier le token JWT
-def verify_token(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        # Votre clé secrète pour le décodage du JWT
-        secret_key = "YOUR_SECRET_KEY"
-        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except PyJWTError:
-        raise credentials_exception
-    return token_data
-
-router = APIRouter(prefix='/data-analysis', tags=["Data Analysis"])
 
 @router.get("/wordcloud-data")
-async def wordcloud_data(token_data: TokenData = Depends(verify_token), which_db: str = Query(enum=["A", "B"])):
+async def wordcloud_data(which_db: str = Query(enum=["A", "B"])):
     try:
+        # Appel de la fonction get_wordcount et récupération des données
         data = get_wordcount(which_db)
         return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
 
 @router.get("/stats")
-async def stats_data( days: int, token_data: TokenData = Depends(verify_token)):
+async def stats_data(days : int):
     try:
+        # Appel de la fonction get_wordcount et récupération des données
         data = get_stats(days)
         return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
