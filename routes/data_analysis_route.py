ec7e8601ba9a2805d168b2225639811fbd79d9aa
valentindjangone <djangone.valentin@gmail.com>
2024-01-02 13:14:30 +0100

    auth

--- routes/data_analysis_route.py
+++ routes/data_analysis_route.py
@@ -1,22 +1,29 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import FastAPI, APIRouter, HTTPException, Query, Depends
+from starlette.requests import Request
 from services.data_analysis_service import get_wordcount, get_stats
 from fastapi_auth_middleware import AuthMiddleware, FastAPIUser
+from typing import Tuple, List
+
+
+# Fonction de vérification de l'autorisation
+def verify_authorization_header(auth_header: str) -> Tuple[List[str], FastAPIUser]:
+    # Ici, implémentez la logique pour décoder et vérifier le JWT
+    # Exemple simple :
+    user = FastAPIUser(first_name="Valentin", last_name="MLOps", user_id=1)
+    scopes = []
+    return scopes, user
+
+app = FastAPI()
+
+# Ajouter le middleware avec votre méthode de vérification à l'application principale
+app.add_middleware(AuthMiddleware, verify_authorization_header=verify_authorization_header)
 
 # Création d'une instance de APIRouter
 router = APIRouter(prefix='/data-analysis', tags=["Data Analysis"])
 
-# Configuration de l'authentification
-auth_middleware = AuthMiddleware(
-    secret_key="password",  # Utilisez votre clé secrète pour les jetons JWT
-    algorithm="HS256",  # Algorithme utilisé pour les jetons JWT
-    auth_header_prefix="Bearer"  # Préfixe utilisé dans le header d'authentification
-)
-
-# Ajout du middleware au routeur
-router.middleware('http')(auth_middleware)
-
 @router.get("/wordcloud-data")
-async def wordcloud_data(user: FastAPIUser, which_db: str = Query(enum=["A", "B"])):
+async def wordcloud_data(request: Request, which_db: str = Query(enum=["A", "B"])):
+    user = request.user  # Accéder à l'utilisateur via l'objet Request
     try:
         data = get_wordcount(which_db)
         return data
@@ -24,9 +31,13 @@ async def wordcloud_data(user: FastAPIUser, which_db: str = Query(enum=["A", "B"
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/stats")
-async def stats_data(user: FastAPIUser, days: int):
+async def stats_data(request: Request, days: int):
+    user = request.user  # Accéder à l'utilisateur via l'objet Request
     try:
         data = get_stats(days)
         return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# Ajouter le routeur à l'application principale
+app.include_router(router)
