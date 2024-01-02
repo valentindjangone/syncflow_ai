442b1f7789dd4ff43fd44517e89e427c8e9649a2
valentindjangone <djangone.valentin@gmail.com>
2024-01-02 12:44:13 +0100

    auth added for data_analysis_route

--- routes/data_analysis_route.py
+++ routes/data_analysis_route.py
@@ -1,23 +1,31 @@
 from fastapi import APIRouter, HTTPException, Query
 from services.data_analysis_service import get_wordcount, get_stats
+from fastapi_auth_middleware import AuthMiddleware, FastAPIUser
 
-router = APIRouter(prefix ='/data-analysis', tags=["Data Analysis"])
+# Création d'une instance de APIRouter
+router = APIRouter(prefix='/data-analysis', tags=["Data Analysis"])
 
+# Configuration de l'authentification
+auth_middleware = AuthMiddleware(
+    secret_key="VotreCleSecrete",  # Utilisez votre clé secrète pour les jetons JWT
+    algorithm="HS256",  # Algorithme utilisé pour les jetons JWT
+    auth_header_prefix="Bearer"  # Préfixe utilisé dans le header d'authentification
+)
+
+# Ajout du middleware au routeur
+router.middleware('http')(auth_middleware)
 
 @router.get("/wordcloud-data")
-async def wordcloud_data(which_db: str = Query(enum=["A", "B"])):
+async def wordcloud_data(user: FastAPIUser, which_db: str = Query(enum=["A", "B"])):
     try:
-        # Appel de la fonction get_wordcount et récupération des données
         data = get_wordcount(which_db)
         return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
 
 @router.get("/stats")
-async def stats_data(days : int):
+async def stats_data(user: FastAPIUser, days: int):
     try:
-        # Appel de la fonction get_wordcount et récupération des données
         data = get_stats(days)
         return data
     except Exception as e:
