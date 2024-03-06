6e483432a4ef81caca787d858e0a8b397995ab65
valentindjangone <127405407+valentindjangone@users.noreply.github.com>
2024-03-06 18:15:47 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,23 +1,19 @@
 from fastapi.testclient import TestClient
-from main import api  # Assurez-vous que ceci importe correctement votre instance FastAPI
+from main import api
 client = TestClient(api)
 
 def test_extract_mission_details():
-    # Données de test
     test_mission = {
         "mission": "I want to sell shoes online. The newly created website must have a trend prediction feature."
     }
 
-    # Envoi de la requête POST à l'endpoint
     response = client.post("/extract_all_details", json=test_mission)
     print(response.text)
 
-    # Vérification du statut de la réponse
     assert response.status_code == 200
 
-    # Vérification de la structure des données de la réponse
     response_data = response.json()
-    assert "detail" in response_data  # ou toute autre clé attendue dans la réponse
+    assert "detail" in response_data
 
 
 
